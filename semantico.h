a4723637d1936801cc1e706c0f30b50be47a51d5
gabrielperné <84864609+gabrielperne@users.noreply.github.com>
2025-01-02 14:33:40 -0300

    Update semantico.h

--- semantico.h
+++ semantico.h
@@ -1,12 +1,36 @@
+#include<stdlib.h>
+#include<stdio.h>
 
-#include "scanner.h"  // Inclua entre aspas
-
-typedef struct {
-    char classe[20];
-    char lexema[50];
-    char tipo[20];
-    int indice;
+typedef struct noPilhaSemantica
+{
     TOKEN token;
-} pilha;
+    int eLinha,eColuna;
+    struct noPilhaSemantica* proximo;
+}NoPilhaSemantica;
 
+typedef struct
+{
+    NoPilhaSemantica* topo;
+    int tamanho;
+}PilhaSemantica;
 
+typedef struct noComum
+{
+    int tipoVar;
+    struct noComum *proximo;
+}NoComum;
+
+typedef struct filaComum
+{
+    NoComum *inicio;
+    NoComum *fim;
+    int tamanho;
+}FilaComum;
+
+extern int contErroSemantico;
+extern FILE* temp;
+void SemanticoIniciar();
+void empilharSemantica(TOKEN);
+void desempilharSemantica();
+void callSemantico(int);
+void finalizarECriarArquivo();
